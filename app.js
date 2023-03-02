34e2e5db5b9c3bcfa1ba66c6d54a2cf2d7e83101
Omar Alshafei <omaralshafei02@gmail.com>
2023-03-01 23:49:55 -0500

    2nd commit

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-    var input = document.querySelector('.input_text');
-    var main = document.querySelector('#name');
-    var temp = document.querySelector('.temp');
-    var desc = document.querySelector('.desc');
-    var clouds = document.querySelector('.clouds');
-    var button= document.querySelector('.submit');
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
 
-    button.addEventListener('click', function(name){
+button.addEventListener('click', function(name){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=08bdb4d67b1acf36d10f60e307e3bebf')
     .then(response => response.json())
     .then(data => {
-    var tempValue = data['main']['temp'];
-    var nameValue = data['name'];
-    var descValue = data['weather'][0]['description'];
-    var fTemp = Math.round(1.8*(tempValue-273)+32)
-    main.innerHTML = nameValue;
-    desc.innerHTML = ""+descValue;
-    temp.innerHTML = fTemp+"\u00B0F";
-    input.value ="";
-
+        var tempValue = data['main']['temp'];
+        var nameValue = data['name'];
+        var descValue = data['weather'][0]['description'];
+        var fTemp = Math.round(1.8*(tempValue-273)+32)
+        main.innerHTML = nameValue;
+        desc.innerHTML = ""+descValue;
+        temp.innerHTML = fTemp+"\u00B0F";
+        input.value ="";
     })
 
     .catch(err => alert("City Name Not Recognized!"));
-    })
+})
